72df76c2ca710affc3a412cfce109abefc644712
ahtrotta <ahtrotta@gmail.com>
2020-08-31 15:09:21 -0400

    completed fixed array

--- medium1/fixed_array.rb
+++ medium1/fixed_array.rb
@@ -1,3 +1,40 @@
+# class FixedArray
+#   - needs 
+#     - initialize method that takes number of elements in array as arg
+#       - create an array of that many elements initialized to nil
+#     - need a getter method and setter method using []
+#     - need a to_s method that just converts the array to a string
+
+class FixedArray
+  def initialize(size)
+    @array = [nil] * size
+  end
+
+  def [](index)
+    array.fetch(index)
+  end
+
+  def []=(index, new_val)
+    if index < array.size
+      array[index] = new_val
+    else
+      raise IndexError
+    end
+  end
+
+  def to_a
+    array.map(&:dup)
+  end
+
+  def to_s
+    array.inspect
+  end
+
+  private
+
+  attr_accessor :array
+end
+
 fixed_array = FixedArray.new(5)
 puts fixed_array[3] == nil
 puts fixed_array.to_a == [nil] * 5
