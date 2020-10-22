c751ccb5970909123bb9008d0e65dfce77d0f340
ahtrotta <ahtrotta@gmail.com>
2020-10-22 17:02:25 -0400

    completed fixed array again

---
+++ medium1/fixed_array_2.rb
@@ -0,0 +1,69 @@
+class FixedArray
+  def initialize(size)
+    @size = size
+    @arr = [nil] * size
+  end
+
+  def to_a
+    @arr.dup
+  end
+
+  def to_s
+    @arr.inspect
+  end
+
+  def [](i)
+    raise IndexError if i > @size - 1 || i < @size * -1
+    @arr[i]
+  end
+
+  def []=(i, val)
+    raise IndexError if i > @size - 1 || i < @size * -1
+    @arr[i] = val
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
