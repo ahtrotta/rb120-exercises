5181dc64616728abd6dca19b310c6b9847d21cf6
ahtrotta <ahtrotta@gmail.com>
2020-10-23 10:08:17 -0400

    completed highest and lower ranking cards again

---
+++ medium1/highest_and_lowest_ranking_cards_2.rb
@@ -0,0 +1,62 @@
+class Card
+  include Comparable
+
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+    @val = get_val
+  end
+
+  def <=>(other)
+    val <=> other.val
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  protected
+  
+  attr_reader :val
+
+  def get_val
+    case @rank
+    when 'Ace'    then 14
+    when 'King'   then 13
+    when 'Queen'  then 12
+    when 'Jack'   then 11
+    when 2..10    then @rank
+    end
+  end
+end
+
+cards = [Card.new(2, 'Hearts'),
+         Card.new(10, 'Diamonds'),
+         Card.new('Ace', 'Clubs')]
+puts cards
+puts cards.min == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
+
+cards = [Card.new(5, 'Hearts')]
+puts cards.min == Card.new(5, 'Hearts')
+puts cards.max == Card.new(5, 'Hearts')
+
+cards = [Card.new(4, 'Hearts'),
+         Card.new(4, 'Diamonds'),
+         Card.new(10, 'Clubs')]
+puts cards.min.rank == 4
+puts cards.max == Card.new(10, 'Clubs')
+
+cards = [Card.new(7, 'Diamonds'),
+         Card.new('Jack', 'Diamonds'),
+         Card.new('Jack', 'Spades')]
+puts cards.min == Card.new(7, 'Diamonds')
+puts cards.max.rank == 'Jack'
+
+cards = [Card.new(8, 'Diamonds'),
+         Card.new(8, 'Clubs'),
+         Card.new(8, 'Spades')]
+puts cards.min.rank == 8
+puts cards.max.rank == 8
